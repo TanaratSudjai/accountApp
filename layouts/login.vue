05c6b4a195aebdfef5319bc97388608b65909bd2
TanaratSudjai <650112418012@gmail.com>
2025-01-26 15:59:07 +0700

    page login register

---
+++ layouts/login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="flex flex-col font-noto bg-cyan-600 h-10 text-white justify-center items-center"
+  >
+    ACCOUNT APPLICATION
+  </div>
+  <slot class="font-noto" />
+</template>
+
+<style scoped>
+.font-noto {
+  font-family: "Noto Sans Thai", sans-serif;
+}
+</style>
