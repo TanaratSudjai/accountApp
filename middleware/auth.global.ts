6662942a004a15f28c50f3a07ecc068fc9faf4c9
TanaratSudjai <650112418012@gmail.com>
2025-01-28 10:32:26 +0700

    auth_middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -2,7 +2,10 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   // ใช้ useState เพื่อกำหนด token แทน
   const tokenCookie = useCookie('token'); // <==ตัวเก่าโดนล้าง cookie ตลอดไม่สามารถนำมาเก็บได้
   const tokenState = useState('token', () => useCookie('token').value);
-  //คำอธิบาย useState('token', () => useCookie('token').value) สร้างสถานะถาวรสำหรับโทเค็น โทเค็นถูกดึงข้อมูลโดยใช้ useCookie('token') และจัดเก็บไว้ในสถานะที่คงอยู่ทั้ง SSR และ CSR ซึ่งจะทำให้แน่ใจได้ว่าเมื่อเพจถูกไฮเดรต (หลังจากกระบวนการ SSR) สถานะโทเค็นจะถูกเติมไว้แล้ว และจะไม่มีการกำหนดไว้
+  //คำอธิบาย useState('token', () => useCookie('token').value) 
+  // สร้างสถานะถาวรสำหรับโทเค็น โทเค็นถูกดึงข้อมูลโดยใช้ useCookie('token') 
+  // และจัดเก็บไว้ในสถานะที่คงอยู่ทั้ง SSR และ CSR ซึ่งจะทำให้แน่ใจได้ว่าเมื่อเพจถูกไฮเดรต 
+  // (หลังจากกระบวนการ SSR) สถานะโทเค็นจะถูกเติมไว้แล้ว และจะไม่มีการกำหนดไว้
 
 
   const token = tokenState.value;
