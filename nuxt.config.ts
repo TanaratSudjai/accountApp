3bd8a49e20fb4b44b789ee516d1ca8da856c2f8a
TanaratSudjai <650112418012@bru.ac.th>
2025-03-24 12:48:54 +0700

    deployed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,25 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "@pinia/nuxt"],
-
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'My Nuxt 3 PWA',
+      short_name: 'Nuxt3PWA',
+      description: 'Progressive Web App with Nuxt 3',
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#ffffff',
+      theme_color: '#000000',
+      icons: [
+        {
+          src: '/icon.png',
+          sizes: '192x192',
+          type: 'image/png'
+        }
+      ]
+    }
+  },
   components: true,
   vue: {
     compilerOptions: {
