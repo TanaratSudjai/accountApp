ec35a45fc777dfe5f861bb77924245e253de85df
ZonixEz <149657519+SonicEz99@users.noreply.github.com>
2025-04-11 13:57:20 +0700

    fix dasktop

--- components/data/dashboardComponent.vue
+++ components/data/dashboardComponent.vue
@@ -1,51 +1,80 @@
 <template>
   <div class="min-h-screen bg-gray-50 py-8">
-    <div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
-
+    <div class="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="mb-8 text-center">
         <h1 class="text-2xl font-bold text-gray-900">ตรวจสอบความถูกต้อง</h1>
         <div class="mt-2 h-1 w-20 bg-indigo-500 mx-auto rounded-full"></div>
       </div>
 
-      <div class="bg-white rounded-lg shadow-lg overflow-hidden">
+      <div class="flex bg-white rounded-lg shadow-lg overflow-hidden justify-around">
         <div class="overflow-x-auto">
+          <h1 class="text-center py-4">รายรับ รายจ่าย</h1>
           <table class="w-full">
             <thead>
               <tr class="bg-gray-50">
-                <th class="px-6 py-4 text-sm font-semibold text-gray-900">ประเภท</th>
-                <th class="px-6 py-4 text-sm font-semibold text-gray-900">สถานะเงิน</th>
-                <th class="px-6 py-4 text-sm font-semibold text-gray-900">แก้ไข</th>
+                <th class="px-6 py-4 text-sm font-semibold text-gray-900">
+                  ประเภท
+                </th>
+                <th class="px-6 py-4 text-sm font-semibold text-gray-900">
+                  รายรับ
+                </th>
+                <th class="px-6 py-4 text-sm font-semibold text-gray-900">
+                  รายจ่าย
+                </th>
+                <th class="px-6 py-4 text-sm font-semibold text-gray-900">
+                  แก้ไข
+                </th>
               </tr>
             </thead>
             <tbody class="divide-y divide-gray-200">
-              <tr v-for="(transition, index) in transition_data" :key="index" class="hover:bg-gray-50 transition-colors duration-200 justify-center">
+              <tr
+                v-for="transition in transition_data.filter(
+                  (i) =>
+                    i.account_category_id === 4 || i.account_category_id === 5
+                )"
+                :key="transition.account_type_id"
+                class="hover:bg-gray-50 transition-colors duration-200 justify-center"
+              >
                 <td class="px-6 py-4 text-sm text-gray-900 flex justify-center">
-                  {{transition.account_type_name}}
+                  {{ transition.account_type_name }}
                 </td>
-                <td class="px-6 py-4 ">
-                  <div :class="['flex justify-center items-center px-4 py-2 rounded-full',
-                      transition.account_type_total < transition.account_type_sum
+                <td class="px-6 py-4">
+                  <div v-if="transition.account_category_id === 4"
+                    :class="[
+                      'flex justify-center items-center px-4 py-2 rounded-full',
+                      transition.account_type_total <
+                      transition.account_type_sum
                         ? 'bg-red-100 text-red-800'
-                        : 'bg-green-100 text-green-800'
-                    ]">
+                        : 'bg-green-100 text-green-800',
+                    ]"
+                  >
                     <span class="text-sm font-medium">
-                      {{transition.account_type_total}}
+                      {{ transition.account_type_total }}
                     </span>
-                    <svg v-if="transition.account_type_total < transition.account_type_sum" class="w-4 h-4 ml-2"
-                      viewBox="0 0 24 24" fill="currentColor">
-                      <path
-                        d="M21,11a1,1,0,0,0-1,1v2.59l-6.29-6.3a1,1,0,0,0-1.42,0L9,11.59,3.71,6.29A1,1,0,0,0,2.29,7.71l6,6a1,1,0,0,0,1.42,0L13,10.41,18.59,16H16a1,1,0,0,0,0,2h5a1,1,0,0,0,.38-.08,1,1,0,0,0,.54-.54A1,1,0,0,0,22,17V12A1,1,0,0,0,21,11Z" />
-                    </svg>
-                    <svg v-else class="w-4 h-4 ml-2" viewBox="0 0 24 24" fill="currentColor">
-                      <path
-                        d="M3,11a1,1,0,0,1,1-1H4v2.59l6.29-6.3a1,1,0,0,1,1.42,0L15,9.59l5.29-5.3a1,1,0,0,1,1.42,1.42l-6,6a1,1,0,0,1-1.42,0L11,8.41,5.41,14H8a1,1,0,0,1,0,2H3a1,1,0,0,1-.38-.08,1,1,0,0,1-.54-.54A1,1,0,0,1,2,15V12A1,1,0,0,1,3,11Z" />
-                    </svg>
+                    
+                  </div>
+                </td>
+                <td>
+                  <div v-if="transition.account_category_id === 5"
+                    :class="[
+                      'flex justify-center items-center px-4 py-2 rounded-full bg-red-100 text-red-800',
+                      transition.account_type_total <
+                      transition.account_type_sum
+                        ? 'bg-red-100 text-red-800'
+                        : 'bg-green-100 text-green-800',
+                    ]"
+                  >
+                    <span class="text-sm font-medium">
+                      {{ transition.account_type_total }}
+                    </span>
+                    
                   </div>
                 </td>
                 <td class="px-6 py-4 flex justify-center">
                   <button
                     class="flex justify-center items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    popovertarget="modal-s">
+                    @click="goToPath(transition.account_category_id)"
+                  >
                     เส้นทาง
                   </button>
 
@@ -53,17 +82,122 @@
                     <div class="bg-white p-6 w-72 space-y-4">
                       <button
                         class="w-full text-left px-4 py-3 rounded-lg hover:bg-yellow-50 border-l-4 border-yellow-500 transition-colors duration-200"
-                        @Click="banklocation()">
+                        @Click="banklocation()"
+                      >
                         ธนาคาร
                       </button>
                       <button
                         class="w-full text-left px-4 py-3 rounded-lg hover:bg-green-50 border-l-4 border-green-500 transition-colors duration-200"
-                        @Click="incomelocation()">
+                        @Click="incomelocation()"
+                      >
                         รายได้
                       </button>
                       <button
                         class="w-full text-left px-4 py-3 rounded-lg hover:bg-red-50 border-l-4 border-red-500 transition-colors duration-200"
-                        @Click="expenselocation()">
+                        @Click="expenselocation()"
+                      >
+                        รายจ่าย
+                      </button>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div class="overflow-x-auto">
+          <h1 class="text-center py-4">ทรัพย์สินส่วนตัว</h1>
+          <table class="w-full">
+            <thead>
+              <tr class="bg-gray-50">
+                <th class="px-6 py-4 text-sm font-semibold text-gray-900">
+                  ประเภท
+                </th>
+                <th class="px-6 py-4 text-sm font-semibold text-gray-900">
+                  เพิ่ม
+                </th>
+                <th class="px-6 py-4 text-sm font-semibold text-gray-900">
+                  ลด
+                </th>
+                <th class="px-6 py-4 text-sm font-semibold text-gray-900">
+                  แก้ไข
+                </th>
+              </tr>
+            </thead>
+            <tbody class="divide-y divide-gray-200">
+              <tr
+              v-for="transition in transition_data.filter(
+                  (i) =>
+                    i.account_category_id === 1 || i.account_category_id === 2 || i.account_category_id === 7
+                )"
+                :key="transition.account_type_id"
+                class="hover:bg-gray-50 transition-colors duration-200 justify-center"
+              >
+                <td class="px-6 py-4 text-sm text-gray-900 flex justify-center">
+                  {{ transition.account_type_name }}
+                </td>
+                <td class="px-6 py-4">
+                  <div v-if="transition.account_type_total >
+                      transition.account_type_sum"
+                    :class="[
+                      'flex justify-center items-center px-4 py-2 rounded-full',
+                      transition.account_type_total <
+                      transition.account_type_sum
+                        ? 'bg-red-100 text-red-800'
+                        : 'bg-green-100 text-green-800',
+                    ]"
+                  >
+                    <span class="text-sm font-medium">
+                      {{ transition.account_type_total }}
+                    </span>
+                  </div>
+                </td>
+
+                <td class="px-6 py-4">
+                  <div  v-if="transition.account_type_total <
+                      transition.account_type_sum"
+                    :class="[
+                      'flex justify-center items-center px-4 py-2 rounded-full',
+                      transition.account_type_total <
+                      transition.account_type_sum
+                        ? 'bg-red-100 text-red-800'
+                        : 'bg-green-100 text-green-800',
+                    ]"
+                  >
+                    <span class="text-sm font-medium">
+                      {{ transition.account_type_total }}
+                    </span>
+                    
+                    
+                  </div>
+                </td>
+                <td class="px-6 py-4 flex justify-center">
+                  <button
+                  @click="goToPath(transition.account_category_id)"
+                    class="flex justify-center items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    เส้นทาง
+                  </button>
+
+                  <div id="modal-s" popover class="rounded-lg shadow-xl">
+                    <div class="bg-white p-6 w-72 space-y-4">
+                      <button
+                        class="w-full text-left px-4 py-3 rounded-lg hover:bg-yellow-50 border-l-4 border-yellow-500 transition-colors duration-200"
+                        @Click="banklocation()"
+                      >
+                        ธนาคาร
+                      </button>
+                      <button
+                        class="w-full text-left px-4 py-3 rounded-lg hover:bg-green-50 border-l-4 border-green-500 transition-colors duration-200"
+                        @Click="incomelocation()"
+                      >
+                        รายได้
+                      </button>
+                      <button
+                        class="w-full text-left px-4 py-3 rounded-lg hover:bg-red-50 border-l-4 border-red-500 transition-colors duration-200"
+                        @Click="expenselocation()"
+                      >
                         รายจ่าย
                       </button>
                     </div>
@@ -85,6 +219,18 @@ const router = useRouter();
 const transition_data = ref([]);
 const loading = ref(true);
 const { $axios } = useNuxtApp();
+
+const goToPath = (category_id) =>{
+  if(category_id === 7){
+    router.push("/transitionbank")
+  }
+  if(category_id === 5){
+    router.push("/transection_expense")
+  }
+  if(category_id === 4){
+    router.push("/transection_income")
+  }
+}
 
 // const fetchData = async () => {
 //   loading.value = true;
@@ -120,7 +266,6 @@ const fetchData = async () => {
   loading.value = false;
 };
 
-
 let intervalId;
 fetchData();
 // onMounted(() => {
@@ -145,5 +290,3 @@ const banklocation = () => navigateTo("/transitionbank");
 const incomelocation = () => navigateTo("/transection_income");
 const expenselocation = () => navigateTo("/transection_expense");
 </script>
-
-
