56fddad3d7c6e3151889e9d38b1699545da038d4
ZonixEz <pnem2003@gmail.com>
2025-05-18 23:51:27 +0700

    make positive

--- components/data/dashboardComponent.vue
+++ components/data/dashboardComponent.vue
@@ -59,7 +59,7 @@
                       v-if="transition.account_category_id === 5"
                       :class="[
                         'flex justify-center items-center px-4 py-2 rounded-full text-sm font-medium',
-                        transition.account_type_total < transition.account_type_sum
+                        transition.account_type_total > transition.account_type_sum
                           ? 'bg-red-100 text-red-800'
                           : 'bg-green-100 text-green-800',
                       ]"
@@ -120,7 +120,7 @@
                       v-if="parseInt(transition.account_type_total) > parseInt(transition.account_type_sum)"
                       class="flex justify-center items-center px-4 py-2 rounded-full bg-amber-100 text-amber-800 text-sm font-medium"
                     >
-                      {{ transition.account_type_difference }}
+                      {{ Math.abs(transition.account_type_difference).toFixed(2) }}
                     </div>
                   </td>
                   <td class="px-6 py-4">
@@ -128,7 +128,7 @@
                       v-if="parseInt(transition.account_type_total) < parseInt(transition.account_type_sum)"
                       class="flex justify-center items-center px-4 py-2 rounded-full bg-blue-100 text-blue-800 text-sm font-medium"
                     >
-                      {{ transition.account_type_difference }}
+                      {{ Math.abs(transition.account_type_difference).toFixed(2) }}
                     </div>
                   </td>
                   <td class="px-6 py-4">
