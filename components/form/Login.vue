ef3635d781115452e3c28100b091d2c4191d634e
TanaratSudjai <tanarat.dev18@gmail.com>
2025-08-01 16:08:57 +0700

    login--

--- components/form/Login.vue
+++ components/form/Login.vue
@@ -1,54 +1,33 @@
 <template>
-  <div
-    ref="boxRef"
-    class="border border-gray-200 p-8 rounded-xl flex flex-col justify-center items-center w-full h-full"
-  >
+  <div ref="boxRef"
+    class="border border-gray-200 p-8 rounded-xl flex flex-col justify-center items-center w-full h-full">
     <text class="text-4xl text-cyan-600 py-5">เข้าสู่ระบบ</text>
-    <form
-      @submit.prevent="handleLogin"
-      class="rounded-md w-full flex flex-col justify-center"
-    >
+    <form @submit.prevent="handleLogin" class="rounded-md w-full flex flex-col justify-center">
       <div class="mb-4">
         <label for="username" class="block text-black font-medium mb-2">
           ชื่อผู้ใช้
         </label>
-        <input
-          v-model="formData.account_user_username"
-          id="username"
-          type="text"
-          placeholder="กรุณากรอกชื่อผู้ใช้"
-          class="p-3 border rounded-md w-full border-cyan-600 focus:ring-2 focus:ring-cyan-600 focus:outline-none"
-        />
+        <input v-model="formData.account_user_username" id="username" type="text" placeholder="กรุณากรอกชื่อผู้ใช้"
+          class="p-3 border rounded-md w-full border-cyan-600 focus:ring-2 focus:ring-cyan-600 focus:outline-none" />
       </div>
       <div class="mb-4">
         <label for="password" class="block text-black font-medium mb-2">
           รหัสผ่าน
         </label>
-        <input
-          v-model="formData.account_user_password"
-          id="password"
-          type="password"
-          placeholder="กรุณากรอกรหัสผ่าน"
-          class="p-3 border rounded-md w-full border-cyan-600 focus:ring-2 focus:ring-cyan-600 focus:outline-none"
-        />
+        <input v-model="formData.account_user_password" id="password" type="password" placeholder="กรุณากรอกรหัสผ่าน"
+          class="p-3 border rounded-md w-full border-cyan-600 focus:ring-2 focus:ring-cyan-600 focus:outline-none" />
       </div>
-      <div v-if="error" class="text-red-500 mb-4">
+      <!-- <div v-if="error" class="text-red-500 mb-4">
         {{ error }}
-      </div>
-      <button
-        @click="handleLogin"
-        :disabled="loading"
-        class="bg-cyan-700 text-white px-4 py-3 rounded-md hover:bg-cyan-600"
-      >
-        {{ loading ? "Logging in..." : "Login" }}
+      </div> -->
+      <button @click="handleLogin" :disabled="loading"
+        class="bg-cyan-700 text-white px-4 py-3 rounded-md hover:bg-cyan-600">
+        {{ loading ? "กำลังเข้าสู่ระบบ..." : "เข้าสู่ระบบ" }}
       </button>
     </form>
-    <button
-      @click="goRegister"
-      class="text-black px-4 py-2 rounded flex gap-2 flex-col md:flex-row lg:flex-row xl:flex-row"
-    >
-      <span class="text-cyan-600">มีชื่อผู้ใช้หรือยัง</span>
-      สมัครใช้งาน
+    <button @click="goRegister"
+      class="text-black px-4 py-2 rounded flex gap-2 flex-col md:flex-row lg:flex-row xl:flex-row ">
+      <span class="text-black ">มีชื่อผู้ใช้หรือยัง ? <span class="text-cyan-600">สมัครใช้งาน</span></span>
     </button>
   </div>
 </template>
@@ -81,6 +60,12 @@ const showAlert = (
     icon,
     confirmButtonText: "ลองอีกครั้ง",
     confirmButtonColor,
+    customClass: {
+      popup: "bg-white text-gt-gray-600 rounded-md shadow-md",
+      title: "text-lg font-normal",
+      content: "text-sm",
+      confirmButton: "px-2 py-2 rounded-md",
+    },
   });
 };
 
@@ -92,7 +77,7 @@ const handleLogin = async () => {
 
   // ตรวจสอบว่าผู้ใช้กรอกข้อมูลครบหรือไม่
   if (!formData.value.account_user_username.trim()) {
-    showAlert("กรุณากรอกชื่อผู้ใช้", "ชื่อผู้ใช้ไม่สามารถเว้นว่างได้");
+    showAlert("เกิดข้อผิดพลาดในการเข้าสู่ระบบ กรุณากรอกชื่อผู้ใช้", "ไม่สามารถเว้นว่างได้ หรือกรอกข้อมูลไม่ถูกต้อง");
     loading.value = false;
     return;
   }
@@ -117,7 +102,7 @@ const handleLogin = async () => {
   } catch (err) {
     error.value =
       err.response?.data?.message || "เกิดข้อผิดพลาดในการเข้าสู่ระบบ";
-    showAlert("เข้าสู่ระบบล้มเหลว", "รหัสผ่านไม่ถูกต้อง");
+    showAlert("เกิดข้อผิดพลาดในการเข้าสู่ระบบ", "ชื่อผู้ใช้ หรือ รหัสผ่านไม่ถูกต้อง");
   } finally {
     loading.value = false;
   }
--- layouts/login.vue
+++ layouts/login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="min-h-screen flex flex-col justify-center items-center">
-    <div class="container mx-auto p-4 lg:w-[40%] xl:w-[40%] font-noto">
+    <div class="container mx-auto font-noto  max-w-md">
       <slot />
     </div>
   </div>
