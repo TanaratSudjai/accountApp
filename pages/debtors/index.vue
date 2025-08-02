426f32e2a03a8a6d81b9c416d280be674f7bb6dc
TanaratSudjai <tanarat.dev18@gmail.com>
2025-08-02 16:16:31 +0700

    UI debtors- refactor

--- pages/debtors/index.vue
+++ pages/debtors/index.vue
@@ -1,37 +1,24 @@
 <!-- SlidingButton.vue -->
 <template>
   <BackComponents />
-  <div class="flex justify-center w-full p-4 ">
+  <div class="max-w-2xl container mx-auto">
     <div class="w-full flex justify-center space-y-2">
-      <div class="relative w-full max-w-2xl">
-        <div
-          class="flex border border-gray-300 rounded-lg overflow-hidden relative"
-        >
+      <div class="relative w-full">
+        <div class="flex border-b border-gray-300  overflow-hidden relative">
           <!-- Background Slider -->
-          <div
-            class="absolute top-0 bottom-0 w-1/2 transition-transform duration-300"
-            :class="{
-              'bg-purple-300 translate-x-0': activeButton === 'borrow',
-              'bg-yellow-200 translate-x-full': activeButton === 'return',
-            }"
-          ></div>
+          <div class="absolute top-0 bottom-0 w-1/2 transition-transform duration-300" :class="{
+            'border-b-2 border-purple-300 translate-x-0': activeButton === 'borrow',
+            'border-b-2 border-yellow-200 translate-x-full': activeButton === 'return',
+          }"></div>
 
-          <button
-            @click="setActive('borrow')"
-            class="relative z-10 flex-1 px-4 py-2 text-center transition-colors duration-300"
-            :class="
-              activeButton === 'borrow' ? 'text-gray-800' : 'text-gray-600'
-            "
-          >
+          <button @click="setActive('borrow')"
+            class="relative z-10 flex-1 px-1 py-1 text-center transition-colors duration-300" :class="activeButton === 'borrow' ? 'text-gray-800' : 'text-gray-600'
+              ">
             ยืม
           </button>
-          <button
-            @click="setActive('return')"
-            class="relative z-10 flex-1 px-4 py-2 text-center transition-colors duration-300"
-            :class="
-              activeButton === 'return' ? 'text-gray-800' : 'text-gray-600'
-            "
-          >
+          <button @click="setActive('return')"
+            class="relative z-10 flex-1 px-1 py-1 text-center transition-colors duration-300" :class="activeButton === 'return' ? 'text-gray-800' : 'text-gray-600'
+              ">
             คืน
           </button>
         </div>
@@ -40,12 +27,12 @@
   </div>
 
   <!-- Conditionally Render Components -->
-  <div class="mt-1 mx-auto max-w-4xl">
+  <div class="mx-auto max-w-2xl">
     <DataTransitionDebtorBorrow v-if="activeButton === 'borrow'" />
     <DataTransitionDebtorReturn v-else />
   </div>
 
-  <div class="mt-1 flex justify-center w-full mb-12">
+  <div class="flex justify-center w-full mb-12">
     <ButtonDashboard />
   </div>
 </template>
