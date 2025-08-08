1ade63b4a74ba021032abad635d95aa8cef00ae8
TanaratSudjai <tanarat.dev18@gmail.com>
2025-08-08 14:13:07 +0700

    --

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,120 @@
 <template>
-  <FormLogin />
+  <div ref="boxRef"
+    class="border border-gray-200 p-8 rounded-xl flex flex-col justify-center items-center w-full h-full">
+    <div class="my-3 flex flex-col justify-center items-center gap-4">
+      <NuxtImg src="./logo.png" alt="" width="80" />
+      <text class="text-xl text-sky-600 py-1">เข้าสู่ระบบ</text>
+    </div>
+    <form @submit.prevent="handleLogin" class="rounded-md w-full flex flex-col justify-center">
+      <div class="mb-4">
+        <label for="username" class="block text-sky-600 font-medium mb-2">
+          ชื่อผู้ใช้
+        </label>
+        <input v-model="formData.account_user_username" id="username" type="text" placeholder="กรุณากรอกชื่อผู้ใช้"
+          class="p-2 border rounded-md w-full border-sky-600 focus:ring-2 focus:ring-sky-600 focus:outline-none" />
+      </div>
+      <div class="mb-4">
+        <label for="password" class="block text-sky-600 font-medium mb-2">
+          รหัสผ่าน
+        </label>
+        <input v-model="formData.account_user_password" id="password" type="password" placeholder="กรุณากรอกรหัสผ่าน"
+          class="p-2 border rounded-md w-full border-sky-600 focus:ring-2 focus:ring-sky-600 focus:outline-none" />
+      </div>
+      <!-- <div v-if="error" class="text-red-500 mb-4">
+        {{ error }}
+      </div> -->
+      <button @click="handleLogin" :disabled="loading"
+        class="bg-sky-600 text-white px-2 py-2 rounded-md hover:bg-sky-500">
+        {{ loading ? "กำลังเข้าสู่ระบบ..." : "เข้าสู่ระบบ" }}
+      </button>
+    </form>
+    <button @click="goRegister"
+      class="text-black px-4 py-2 rounded flex gap-2 flex-col md:flex-row lg:flex-row xl:flex-row ">
+      <span class="text-black ">มีชื่อผู้ใช้หรือยัง ? <span class="text-sky-600">สมัครใช้งาน</span></span>
+    </button>
+  </div>
 </template>
 
 <script setup>
 definePageMeta({
   layout: "login",
 });
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import Swal from "sweetalert2";
+
+const { $axios } = useNuxtApp();
+const boxRef = ref(null);
+const router = useRouter();
+const loading = ref(false);
+const error = ref("");
+
+const formData = ref({
+  account_user_username: "",
+  account_user_password: "",
+});
+
+const showAlert = (
+  title,
+  text,
+  icon = "error",
+  confirmButtonColor = "#0891b2"
+) => {
+  Swal.fire({
+    title,
+    text,
+    icon,
+    confirmButtonText: "ลองอีกครั้ง",
+    confirmButtonColor,
+    customClass: {
+      popup: "bg-white text-gt-gray-600 rounded-md shadow-md",
+      title: "text-lg font-normal",
+      content: "text-sm",
+      confirmButton: "px-2 py-2 rounded-md",
+    },
+  });
+};
+
+const handleLogin = async () => {
+  if (loading.value) return;
+
+  loading.value = true;
+  error.value = "";
+
+  // ตรวจสอบว่าผู้ใช้กรอกข้อมูลครบหรือไม่
+  if (!formData.value.account_user_username.trim()) {
+    showAlert("เกิดข้อผิดพลาดในการเข้าสู่ระบบ กรุณากรอกชื่อผู้ใช้", "ไม่สามารถเว้นว่างได้ หรือกรอกข้อมูลไม่ถูกต้อง");
+    loading.value = false;
+    return;
+  }
+
+  if (!formData.value.account_user_password.trim()) {
+    showAlert("กรุณากรอกรหัสผ่าน", "รหัสผ่านไม่สามารถเว้นว่างได้");
+    loading.value = false;
+    return;
+  }
+  console.log(formData.value);
+
+  try {
+    const response = await $axios.post("/auth/login", {
+      account_user_username: formData.value.account_user_username,
+      account_user_password: formData.value.account_user_password,
+    });
+
+    const token = response.data.token;
+    localStorage.setItem("token", token);
+    await router.push("/home");
+    window.location.reload();
+  } catch (err) {
+    error.value =
+      err.response?.data?.message || "เกิดข้อผิดพลาดในการเข้าสู่ระบบ";
+    showAlert("เกิดข้อผิดพลาดในการเข้าสู่ระบบ", "ชื่อผู้ใช้ หรือ รหัสผ่านไม่ถูกต้อง");
+  } finally {
+    loading.value = false;
+  }
+};
+
+const goRegister = async () => {
+  router.push("/register");
+};
 </script>
--- pages/register/index.vue
+++ pages/register/index.vue
@@ -1,9 +1,141 @@
 <template>
-    <FormRegister />
+  <div ref="boxRegister"
+    class="  p-2 px-12  rounded-xl flex flex-col justify-center items-center border border-gray-200">
+    <div class="my-3 flex flex-col justify-center items-center gap-4">
+      <NuxtImg src="./logo.png" alt="" width="80" />
+      <text class="text-xl text-sky-600 py-1">สมัครใช้งานระบบ</text>
+    </div>
+    <form @submit.prevent="handleLogin" class="rounded w-full flex flex-col gap-2 justify-center ">
+      <div class="">
+        <label for="username" class="block text-sky-600 font-medium ">
+          ชื่อบัญชี
+        </label>
+        <input v-model="formData.account_user_name" id="name" type="text" placeholder="ชื่อผู้ใช้"
+          class="p-2 rounded-lg w-full border-sky-600 focus:ring-2 focus:ring-sky-600 focus:outline-none border" />
+      </div>
+
+      <div class="">
+        <label for="username" class="block text-sky-600 font-medium ">
+          ชื่อผู้ใช้
+        </label>
+        <input v-model="formData.account_user_username" id="username" type="email" placeholder="ชื่อผู้ใช้"
+          class="p-2 rounded-lg w-full border-sky-600 focus:ring-2 focus:ring-sky-600 focus:outline-none border" />
+      </div>
+
+      <div class="">
+        <label for="password" class="block text-sky-600 font-medium ">
+          รหัสผ่าน
+        </label>
+        <input v-model="formData.account_user_password" id="password" type="password" placeholder="รหัสผ่าน"
+          class="p-2 rounded-lg w-full border-sky-600 focus:ring-2 focus:ring-sky-600 focus:outline-none border" />
+      </div>
+      <div class="">
+        <label for="password" class="block text-sky-600 font-medium ">
+          ยืนยันรหัสผ่านอีกครั้ง
+        </label>
+        <input v-model="formData.account_user_confirmpassword" id="password" type="password"
+          placeholder="ยืนยันรหัสผ่านอีกครั้ง"
+          class="p-2 rounded-lg w-full  border-sky-600 focus:ring-2 focus:ring-sky-600 focus:outline-none border" />
+      </div>
+
+      <button :disabled="isDisabled" :class="[
+        'bg-sky-600 text-white p-2 rounded-lg hover:bg-sky-600 transition',
+        { 'opacity-50 cursor-not-allowed': isDisabled }
+      ]" type="submit">
+        สมัครใช้งาน
+      </button>
+    </form>
+    <button @click="goLogin" class="text-sky-600 px-3 py-2 rounded ">
+      <span class="text-gray-600">มีชื่อผู้ใช้อยู่เเล้ว</span> เข้าสู่ระบบ
+    </button>
+  </div>
 </template>
 
 <script setup>
 definePageMeta({
   layout: "login",
 });
+import { ref, reactive } from "vue";
+import { useRouter } from "vue-router";
+import Swal from "sweetalert2";
+import { gsap } from 'gsap'
+const boxRegister = ref(null)
+
+
+
+const { $axios } = useNuxtApp();
+const loading = ref(false);
+const error = ref("");
+const router = useRouter();
+const formData = reactive({
+  account_user_name: "",
+  account_user_username: "",
+  account_user_password: "",
+  account_user_confirmpassword: "",
+});
+
+
+const isDisabled = computed(() => {
+  return (
+    !formData.account_user_name ||
+    !formData.account_user_username ||
+    !formData.account_user_password ||
+    !formData.account_user_confirmpassword
+  );
+});
+
+
+
+const showAlert = (title, text, icon = "error", confirmButtonColor = "#0891b2") => {
+  Swal.fire({
+    title,
+    text,
+    icon,
+    confirmButtonText: "เข้าสู่ระบบ",
+    confirmButtonColor,
+    customClass: {
+      popup: "bg-white text-gt-gray-600 rounded-md shadow-md",
+      title: "text-lg font-normal",
+      content: "text-sm",
+      confirmButton: "px-2 py-2 rounded-md",
+    },
+  });
+};
+const handleLogin = async () => {
+  if (loading.value) return;
+
+  loading.value = true;
+  error.value = "";
+
+  const { account_user_name, account_user_username, account_user_password, account_user_confirmpassword } = formData;
+
+  try {
+    if (account_user_password !== account_user_confirmpassword) {
+      showAlert("เกิดข้อผิดพลาดในการสมัครสมาชิก กรุณาลองอีกครั้ง", "รหัสผ่านและยืนยันรหัสผ่านไม่ตรงกัน");
+      return;
+    }
+
+    await $axios.post("/auth/register", {
+      account_user_name,
+      account_user_username,
+      account_user_password,
+    });
+
+    showAlert("สมัครสำเร็จ!", "คุณสามารถเข้าสู่ระบบได้ทันที", "success",);
+    await router.push("/");
+  } catch (err) {
+    if (err.response && err.response.status === 400) {
+      showAlert("เกิดข้อผิดพลาดในการสมัครสมาชิก", "ชื่อผู้ใช้หรืออีเมลนี้ถูกใช้งานแล้ว");
+      return;
+    }
+    showAlert("เกิดข้อผิดพลาด!", "ไม่สามารถสมัครใช้งานได้");
+    console.error(err);
+  } finally {
+    loading.value = false;
+  }
+};
+
+const goLogin = () => {
+  router.push("/");
+};
 </script>
