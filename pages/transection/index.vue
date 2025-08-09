145283650f11a99b75c6996c954fd78daf8213cc
TanaratSudjai <tanarat.dev18@gmail.com>
2025-08-09 21:34:31 +0700

    --

--- pages/transection/index.vue
+++ pages/transection/index.vue
@@ -300,7 +300,7 @@ const submitDifferences = async () => {
 const fetchDataTransitionOpen = async () => {
   try {
     const token = localStorage.getItem("token");
-    const response = await $axios.get(`/data_transition_open`);
+    const response = await $axios.get(`/transition-summary`);
     const data = response.data.data;
 
     // Set all data from single API response
