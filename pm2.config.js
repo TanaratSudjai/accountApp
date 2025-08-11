edee2b6363b23a05e41a5cce11274fc516fae566
TanaratSudjai <tanarat.dev18@gmail.com>
2025-08-11 23:02:21 +0700

    deploy V 0.1

---
+++ pm2.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: "accountApp",
+      script: ".output/server/index.mjs",
+      cwd: "/home/xver/accountApp",
+      interpreter: "node"
+    }
+  ]
+}
