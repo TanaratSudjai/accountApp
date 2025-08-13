ebd0f79e3ff966575129721eacf3df5edc217894
TanaratSudjai <tanarat.dev18@gmail.com>
2025-08-13 20:11:39 +0700

    deploy V0.1

--- pm2.config.js
+++ pm2.config.js
@@ -1,13 +1,18 @@
 module.exports = {
   apps: [
     {
-      name: "accountApp",
-      script: ".output/server/index.mjs",
-      cwd: "/home/xver/accountApp",
-      interpreter: "node",
+      name: "my-app",
+      script: "src/index.js",
+      instances: "max",
+      exec_mode: "cluster",
       env: {
-        NODE_ENV: "production",
+        NODE_ENV: "development",
+        PORT: 3000
       },
-    },
-  ],
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 8080
+      }
+    }
+  ]
 };
