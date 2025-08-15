d5e97a0fef218ef0b26386eefb9f78b7c24cb86c
TanaratSudjai <tanarat.dev18@gmail.com>
2025-08-15 23:00:05 +0700

    oftimize - axios

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -3,42 +3,13 @@ import { useCookie } from "#app";
 
 export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig();
-  const tokenCookie = useCookie("token", {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 2 ชั่วโมง
-    secure: true, // ใช้เฉพาะ HTTPS
-    sameSite: "None",
-    path: "/",
-    httpOnly: true,
-    domain: config.public.cookieDomain,
-  });
 
   const $axios = axios.create({
-    baseURL: config.public.apiBase,
-    withCredentials: true,
+    baseURL: config.public.apiBase, 
+    withCredentials: true,          
     timeout: 10000,
   });
 
-  $axios.interceptors.request.use(
-    (config: any) => {
-      if (tokenCookie.value) {
-        config.headers.Authorization = `Bearer ${tokenCookie.value}`;
-      }
-      return config;
-    },
-    (error) => Promise.reject(error)
-  );
-
-  $axios.interceptors.response.use(
-    (response) => {
-      if (response.config.url?.includes("/") && response.data.token) {
-        tokenCookie.value = response.data.token;
-      }
-      return response;
-    },
-    (error) => {
-      return Promise.reject(error);
-    }
-  );
 
   return {
     provide: {
